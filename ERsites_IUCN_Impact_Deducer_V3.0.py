c386157d5c6b940558763aa7a1915fe36cb70106
kusasasvulcan <KusasaS@vulcan.com>
2020-08-05 09:38:31 +0200

    version 3.0

--- ERsites_IUCN_Impact_Deducer_V3.0.py
+++ ERsites_IUCN_Impact_Deducer_V3.0.py
@@ -1,27 +1,32 @@
-'''-------------------------------------------------------------------------
-Script Name:      ER Sites Impact Deducer
-Version:          2.0
-Description:      This tool automates deduces the IUCN endangered animal
-                    species per ER Site.
+'''---------------------------------------------------------------------------------
+Script Name:      ER Sites IUCN Impact Deducer
+Version:          3.0
+Description:      This tool automates the deduction of the IUCN terrestrial mammal
+                    species per ER Site (hence the ER Sites IUCN Impact).
 Created By:       Kusasalethu Sithole
 Created Date:     2020-07-06
 Last Revised By:  Kusasalethu Sithole
-Last Revision:    2020-07-26
--------------------------------------------------------------------------'''
+Last Revision:    2020-08-05
+---------------------------------------------------------------------------------'''
+
+
+print("\n\nTOOL - ER Sites Impact Deducer")
+print("\nReminder - For this tool to execute successfully, your machine needs:\n\t 1) the pip package and have its bin directory mapped in the machines 'path' system environment variable.\n\t 2) The geopandas, psycopg2 libraries installed using your pip package.")
+
 
 #Import Libraries
 import datetime
 import geopandas as gpd
 import os
+import pandas as pd
 import psycopg2
+from sqlalchemy import create_engine
 import time
 
-print("\n\nTOOL - ER Sites Impact Deducer")
-print("\nReminder - For this tool to execute successfully, your machine needs:\n\t 1) the pip package and have its bin directory mapped in the machines 'path' system environment variable.\n\t 2) The geopandas library installed using your pip package.")
 
-#---------------------------Declaring Data Paths------------------
+#-----------------------------------Declaring Data Paths--------------------------
 ## Working Directory
-working_directory = input("\nAbsolute path of the working directory: ")
+working_directory = input("\nAbsolute path of the working directory which will receive the output files: ")
 if working_directory == "":
     print("\nREMINDER: When running this tool, remember to specify the absolute path of the working directory according to request above.")
     time.sleep(10)
@@ -38,14 +43,53 @@ pgdatabase = input("Postgres Database Name: ")
 pguser = input("Postgres Username: ")
 pgpassword = input("Postgres User Password: ")
     
+postgres_engine = create_engine('postgresql://' + pguser + ':' + pgpassword + '@' + pghost + ':' + pgport + '/' + pgdatabase)
 pg_con = psycopg2.connect(host = pghost, database = pgdatabase, port = pgport, user = pguser, password = pgpassword)
 
+## State target IUCN category
+sql_iucn_categories = "SELECT DISTINCT(category) status FROM terrestrial_mammals;"
+categories = pd.read_sql_query(sql_iucn_categories,con=postgres_engine)
 
-#---------------------------Preparing the environment------------------
+print("\n\nThe existing IUCN categories are:")
+for row in categories["status"]:
+    print("\t " + str(row))
+
+target_category_key = input("Please state the exact name of your IUCN category of interest (MANDATORY): ")
+if target_category_key == "" or target_category_key == " ":
+    print("REMINDER: When running this tool, remember to specify the name of your IUCN category of interest according to request above.")
+    time.sleep(10)
+    exit()
+
+row_no = len(categories)
+iter_no = 0
+for row in categories["status"]:
+    if row == target_category_key:
+        break 
+    elif  iter_no < row_no - 1:
+        iter_no += 1
+        continue
+    else:
+        print("REMINDER: Ensure that the category name provided above is both typed correctly (Font case sensitive) and the category actually exists in the IUCN layer.")
+        time.sleep(10)
+        exit()
+
+category_names = {"LC":"Least Concern",
+                 "EX":"Extinct",
+                 "CR":"Critically Endangered",
+                 "EN":"Endangered",
+                 "VU":"Vulnerable",
+                 "DD":"Data Deficient",
+                 "EW":"Extinct in the Wild",
+                 "NT":"Near Threatened"}
+
+target_category_name = category_names[target_category_key]
+
+
+#-----------------------------------Preparing the environment------------------------------
 os.chdir(working_directory)
 
 
-#---------------------------Defining common custom functions------------------
+#-----------------------------------Defining common custom functions-----------------------
 def currentSecondsTime():
     """ Returns the current time in seconds"""
     return int(time.time())
@@ -82,23 +126,23 @@ sites_mammals.to_file(".\ER_Sites_IUCN_Terrestrial_Mammals.shp")
 S1_endTime = currentSecondsTime()
 showPyMessage("\n -- Step 1 completed successfully. Step took {}. ".format(timeTaken(S1_startTime, S1_endTime)))
 
-# ---------------------------STEP 2: Filtering only the Critically Endangered Terrestrial Mammals that overlap the ER Sites---------------------------
-print("\nStep 2: Filtering only the Critically Endangered Terrestrial Mammals that overlap the ER Sites.")
+# ---------------------------STEP 2: Filtering only the Terrestrial Mammals of the user target category that overlap the ER Sites---------------------------
+print("\nStep 2: Filtering only the {} Terrestrial Mammals that overlap the ER Sites.".format(target_category_name))
 S2_startTime = currentSecondsTime()
 
-sql_2 = "SELECT a.name er_site, b.binomial species, CASE WHEN b.category = 'CR' THEN 'Critically Endangered' ELSE '' END status, a.geom geom FROM er_sites a, terrestrial_mammals b WHERE st_intersects(a.geom, b.geom) AND UPPER(b.category)='CR';"
+sql_2 = "SELECT a.name er_site, b.binomial species, CASE WHEN b.category = '{0}' THEN '{1}' ELSE '' END status, a.geom geom FROM er_sites a, terrestrial_mammals b WHERE st_intersects(a.geom, b.geom) AND UPPER(b.category)='{0}';".format(target_category_key, target_category_name)
 filtered_sites_mammals = gpd.GeoDataFrame.from_postgis(sql_2, pg_con, geom_col='geom' )
-filtered_sites_mammals.to_file(".\Critically_Endangered_ER_Sites_IUCN_Terrestrial_Mammals.shp")
+filtered_sites_mammals.to_file(".\{}_ER_Sites_IUCN_Terrestrial_Mammals.shp".format(target_category_name))
 
 S2_endTime = currentSecondsTime()
 showPyMessage("\n -- Step 2 completed successfully. Step took {}. ".format(timeTaken(S2_startTime, S2_endTime)))
     
     
-# ---------------------------STEP 3: Summing the filtered Critically Endangered Terrestrial Mammals per ER Site---------------------------
-print("\nStep 3: Summing the filtered Critically Endangered Terrestrial Mammals per ER Site.")
+# ---------------------------STEP 3: Summing the filtered Terrestrial Mammals of the user target category per ER Site---------------------------
+print("\nStep 3: Summing the filtered {} Terrestrial Mammals per ER Site.".format(target_category_name))
 S3_startTime = currentSecondsTime()
 
-sql_3 = "WITH site_CR_T_Mammals AS (SELECT a.name er_site, b.binomial species, CASE WHEN b.category = 'CR' THEN 'Critically Endangered' ELSE '' END status, a.geom geom FROM er_sites a, terrestrial_mammals b WHERE st_intersects(a.geom, b.geom) AND UPPER(b.category)='CR') SELECT MIN(er_site) er_site, MIN(status) status, COUNT(er_site) IUCN_Impact, MIN(geom) geom FROM site_CR_T_Mammals GROUP BY er_site;"
+sql_3 = "WITH site_CR_T_Mammals AS (SELECT a.name er_site, b.binomial species, CASE WHEN b.category = '{0}' THEN '{1}' ELSE '' END status, a.geom geom FROM er_sites a, terrestrial_mammals b WHERE st_intersects(a.geom, b.geom) AND UPPER(b.category)='{0}') SELECT MIN(er_site) er_site, MIN(status) status, COUNT(er_site) IUCN_Impact, MIN(geom) geom FROM site_CR_T_Mammals GROUP BY er_site;".format(target_category_key, target_category_name)
 summed_filtered_sites_mammals = gpd.GeoDataFrame.from_postgis(sql_3, pg_con, geom_col='geom' )
 summed_filtered_sites_mammals["Species"] = ""
 
